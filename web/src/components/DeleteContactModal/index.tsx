bbd02787a14784966f7361432895993b40087a95
Garcez17 <ggarcez613@gmail.com>
2022-02-24 18:47:49 -0300

    fix: redirect after delete contact

--- web/src/components/DeleteContactModal/index.tsx
+++ web/src/components/DeleteContactModal/index.tsx
@@ -31,7 +31,7 @@ export function DeleteContactModal({
   async function handleDeleteContact() {
     await deleteContact.mutateAsync();
 
-    if (!isHome) router.push('/');
+    if (router.asPath !== '/') router.push('/');
 
     onRequestClose();
   }
@@ -44,18 +44,18 @@ export function DeleteContactModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <div className="flex flex-col gap-8 bg-gray-900 p-4 w-96 h-96 rounded items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-8 p-4 bg-gray-900 rounded w-96 h-96">
         <h2 className="text-2xl">Deseja excluir esse contato?</h2>
         <FiTrash className="w-32 h-32 text-gray-500" />
-        <div className="flex gap-4 items-center justify-center w-full px-6">
+        <div className="flex items-center justify-center w-full gap-4 px-6">
           <button
-            className="bg-red-500 py-4 rounded flex-1 duration-100 hover:bg-red-600"
+            className="flex-1 py-4 duration-100 bg-red-500 rounded hover:bg-red-600"
             onClick={onRequestClose}
           >
             Não
           </button>
           <button
-            className="bg-blue-500 py-4 rounded flex-1 duration-100 hover:bg-blue-600"
+            className="flex-1 py-4 duration-100 bg-blue-500 rounded hover:bg-blue-600"
             onClick={handleDeleteContact}
           >
             Sim
--- web/src/pages/contatos/[id].tsx
+++ web/src/pages/contatos/[id].tsx
@@ -20,8 +20,8 @@ export default function Contact({ id }: ContactProps) {
   return (
     <div className="flex flex-col flex-1">
       {isLoading || !data ? (
-        <div className="flex-1 flex items-center justify-center">
-          <svg className="animate-spin h-5 w-5 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <div className="flex items-center justify-center flex-1">
+          <svg className="w-5 h-5 mr-3 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
           </svg>
         </div>
@@ -29,9 +29,9 @@ export default function Contact({ id }: ContactProps) {
         <>
           <DeleteContactModal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} contactId={data.contact.id} />
 
-          <header className="h-60 flex flex-col gap-4 items-center justify-center p-4">
+          <header className="flex flex-col items-center justify-center gap-4 p-4 h-60">
             <button
-              className="flex self-start items-center justify-center w-8 h-8 duration-100 rounded-full hover:bg-gray-800"
+              className="flex items-center self-start justify-center w-8 h-8 duration-100 rounded-full hover:bg-gray-800"
               type="button"
               onClick={() => router.back()}
             >
@@ -39,9 +39,9 @@ export default function Contact({ id }: ContactProps) {
             </button>
 
             <Image className="rounded-3xl" src={data.contact.avatar_url} width={150} height={110} alt={data.contact.name} />
-            <h1 className="text-bold text-2xl">{data.contact.name}</h1>
+            <h1 className="text-2xl text-bold">{data.contact.name}</h1>
           </header>
-          <section className="bg-gray-800 flex-1 rounded-t-3xl px-6 flex flex-col divide-y divide-gray-900">
+          <section className="flex flex-col flex-1 px-6 bg-gray-800 divide-y divide-gray-900 rounded-t-3xl">
             <div className="flex flex-col gap-2 py-6">
               <span className="text-gray-500">Telefone</span>
               <p>{data.contact.phone_number}</p>
@@ -51,17 +51,17 @@ export default function Contact({ id }: ContactProps) {
               <p>{data.contact.email}</p>
             </div>
 
-            <div className="flex flex-1 gap-4 items-center justify-center w-full px-6">
+            <div className="flex items-center justify-center flex-1 w-full gap-4 px-6">
               <Link href={`/editar-contato/${data.contact.id}`}>
-                <a className="bg-blue-500 flex items-center justify-center w-32 h-32 rounded flex-1 duration-100 hover:bg-blue-600">
-                  <FiEdit3 className="h-10 w-10" />
+                <a className="flex items-center justify-center flex-1 w-32 h-32 duration-100 bg-blue-500 rounded hover:bg-blue-600">
+                  <FiEdit3 className="w-10 h-10" />
                 </a>
               </Link>
               <button
-                className="bg-red-500 flex items-center justify-center w-32 h-32 rounded flex-1 duration-100 hover:bg-red-600"
+                className="flex items-center justify-center flex-1 w-32 h-32 duration-100 bg-red-500 rounded hover:bg-red-600"
                 onClick={() => setModalIsOpen(true)}
               >
-                <FiTrash className="h-10 w-10" />
+                <FiTrash className="w-10 h-10" />
               </button>
             </div>
           </section>
